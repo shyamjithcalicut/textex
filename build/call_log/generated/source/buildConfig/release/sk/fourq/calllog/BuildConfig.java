c33175f7bec06e535a4114b941133a17e8029761
Shyamjith Cherukara <shyamjith@outlook.com>
2021-04-09 00:44:17 +0530

    Initial Commit

---
+++ build/call_log/generated/source/buildConfig/release/sk/fourq/calllog/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package sk.fourq.calllog;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "sk.fourq.calllog";
+  public static final String BUILD_TYPE = "release";
+}
